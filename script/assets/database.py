2a477242d6c77e93f290791c8a852678ca2b5808
Sandro Pernerstorfer <sandro.pernerstorfer@gmail.com>
2021-10-18 15:52:31 +0200

    console color codes added into database

--- script/assets/database.py
+++ script/assets/database.py
@@ -1,6 +1,15 @@
 # Import Typical Functions
 from assets.typicals.P_AinHART import *
 
+consoleColorCodes = {
+  '0':      '0',
+  'blue':   '94',
+  'red':    '91',
+  'yellow': '93',
+  'green':  '92',
+  'cyan':   '96'
+}
+
 safetyAreas = {
   '+H06': 'Distn'
 }

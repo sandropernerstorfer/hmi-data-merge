73a878f0f137fad5d2b82aefa81e2b7015960d72
Sandro Pernerstorfer <sandro.pernerstorfer@gmail.com>
2021-10-18 23:00:52 +0200

    database commented

--- script/assets/database.py
+++ script/assets/database.py
@@ -1,21 +1,21 @@
 from assets.filtering.P_AinHART import *
 
 #
-# Available Safety Areas
+# --------------------------------------------------------------------------------------------------------- Available Safety Areas
 #
 safetyAreas = {
   '+H06': 'Distn'
 }
 
 #
-# Available Typicals
+# --------------------------------------------------------------------------------------------------------- Available Typicals
 #
 typicals = {
   'P_AinHART': [P_AinHART_Filter, ['AI', 'DI', 'FI', 'LI', 'PDI', 'PI', 'SI', 'TI', 'XI']]
 }
 
 #
-# Sheet Row & Column Locations
+# --------------------------------------------------------------------------------------------------------- Sheet Row & Column Locations
 #
 firstRow          = 8
 pidColumn         = 3

d3df3e54d2a8e9a4a8f8e40e22a991b738986713
Sandro Pernerstorfer <sandro.pernerstorfer@gmail.com>
2021-10-18 22:32:13 +0200

    pid row and col locations written into database

--- script/assets/database.py
+++ script/assets/database.py
@@ -6,9 +6,16 @@ from assets.filtering.P_AinHART import *
 safetyAreas = {
   '+H06': 'Distn'
 }
+
 #
 # Available Typicals
 #
 typicals = {
   'P_AinHART': [P_AinHART_Filter, ['AI', 'DI', 'FI', 'LI', 'PDI', 'PI', 'SI', 'TI', 'XI']]
 }
+
+#
+# Sheet Row & Column Locations
+#
+firstRow  = 8
+pidColumn = 3
--- script/assets/filtering/__filterUtils.py
+++ script/assets/filtering/__filterUtils.py
@@ -1,18 +1,16 @@
 def getAllWithPid(pid, master_ws):
   
+  from assets.database import firstRow, pidColumn
+  
   try: pid = int(pid)
   except: pid = pid
   
-  firstRow  = 8
-  pidCol    = 3
-  
   elements = []
-  
   for key, *values in master_ws.iter_rows(min_row = firstRow):
-    if(key.value == None):  # Stop if row A has no value
+    if(key.value == None):
       break
 
-    if(values[pidCol-2].value == pid):  # Find row with defined PID
+    if(values[pidColumn-2].value == pid):
       row = [v.value for v in values]
       elements.append(row)
   

c1e3181c448382fba3734c79ae07fc856e0c3d55
Sandro Pernerstorfer <sandro.pernerstorfer@outlook.com>
2023-09-17 14:11:10 +0200

    refactor powershell deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,12 +1,16 @@
-Read-Host -Prompt 'Make sure everything is pushed'
-
+# save needed directory paths
 $mainDir  = Split-Path $MyInvocation.MyCommand.Path -Parent
 $srcDir   = "$mainDir/src"
 $icon 	  = "$mainDir/icon.ico"
-$tempDir  = "$mainDir\temp-py-src"
-$distDir  = "$tempDir\dist"
+$tempDir  = "$mainDir/temp-src"
+$distDir  = "$mainDir/temp-src/dist"
 $exeCMD   = "pyinstaller --onefile --icon=icon.ico main.py"
 $appName  = "HMIDataMerge"
+
+# git check
+if(Test-Path -Path "$mainDir/.git"){
+	Read-Host -Prompt 'Caution: Make sure everything is pushed before proceeding'
+}
 
 # create temp dir + files
 Copy-Item -Path $srcDir -Destination $tempDir -Recurse
@@ -19,27 +23,27 @@ try {
 }
 catch {
 	Write-Host "Error: build failed"
-	Start-Sleep -s 3
+	Write-Host "Make sure you have 'pyinstaller' installed"
+	Start-Sleep -s 4
 	Exit
 }
 
+Set-Location $mainDir
+
 # move old .zip and .exe to trash
-try{
-	Remove-ItemSafely "$mainDir/*.zip"
-	Remove-ItemSafely "$mainDir/*.exe"
-}
-catch {}
+Remove-Item "$mainDir/*.zip"
+Remove-Item "$mainDir/*.exe"
 
 # name new exe - compress to zip - and move into main dir
-Rename-Item -Path "$distDir\*.exe" -NewName "$appName.exe"
-Copy-Item -Path "$distDir\*.exe" -Destination $mainDir
-Compress-Archive -Path "$distDir\*.exe" -Destination "$mainDir\$appName.zip"
+Rename-Item -Path "$distDir\main.exe" -NewName "$appName.exe"
+Copy-Item -Path "$distDir\$appName.exe" -Destination $mainDir
+Compress-Archive -Path "$distDir\$appName.exe" -Destination "$mainDir\$appName.zip"
 
 # remove temp dir
-Remove-ItemSafely $tempDir
+Get-ChildItem -Path $tempDir -Recurse | Remove-Item -force -recurse
+Remove-Item $tempDir -Force
 
 # github actions
-Set-Location $mainDir
 $date   = Get-Date -Format "dd/MM/yyyy HH:mm"
 Invoke-Expression "git status"
 Read-Host -Prompt "continue?"
